c02c42c480f4281cf63fe4fc412d00b7466316d6
Shraddha Jain <shraddhajain0989@gmail.com>
2026-04-13 12:43:58 +0530

    final deployment fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Midnight-monk/',   // ⭐ ADD THIS LINE
   server: {
     host: true,
     port: 5173,
